034fdad23750ba45d40a1800a1bb812c24a748ce
Otto777L <otto.lh12@gmail.com>
2023-01-21 01:23:16 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let iconRdm = Math.floor(Math.random() * 4) + 1;
+  let txtRdm = Math.floor(Math.random() * 12); //document.getElementById("number").innerHTML;
+  switch (iconRdm) {
+    case 1:
+      document.getElementById("up").innerHTML = "♦";
+      document.getElementById("bot").innerHTML = "♦";
+      break;
+    case 2:
+      document.getElementById("up").innerHTML = "♥";
+      document.getElementById("bot").innerHTML = "♥";
+      break;
+    case 3:
+      document.getElementById("up").innerHTML = "♠";
+      document.getElementById("bot").innerHTML = "♠";
+      break;
+    case 4:
+      document.getElementById("up").innerHTML = "♣";
+      document.getElementById("bot").innerHTML = "♣";
+      break;
+    default:
+      break;
+  }
+  if (0 < txtRdm && txtRdm < 10) {
+    document.getElementById("number").innerHTML = txtRdm;
+  } else if (txtRdm == 0) {
+    document.getElementById("number").innerHTML = "A";
+  } else if (txtRdm == 10) {
+    document.getElementById("number").innerHTML = "J";
+  } else if (txtRdm == 11) {
+    document.getElementById("number").innerHTML = "Q";
+  } else if (txtRdm == 12) {
+    document.getElementById("number").innerHTML = "A";
+  }
 };
