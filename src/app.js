ece29a9ced04453731a0939344eaf56785806b25
Otto777L <otto.lh12@gmail.com>
2023-01-23 20:55:12 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -5,38 +5,132 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let buttonh = document.getElementById("height");
+let buttonw = document.getElementById("width");
+const cardContainer = document.querySelector(".card");
+buttonh.addEventListener("change", function changeHeight(event) {
+  cardContainer.style.height = event.target.value + "px";
+});
+buttonw.addEventListener("change", function changeWidth(event) {
+  cardContainer.style.width = event.target.value + "px";
+});
+
 window.onload = function() {
   let iconRdm = Math.floor(Math.random() * 4) + 1;
-  let txtRdm = Math.floor(Math.random() * 12); //document.getElementById("number").innerHTML;
+  let txtRdm = Math.floor(Math.random() * 12);
   switch (iconRdm) {
     case 1:
       document.getElementById("up").innerHTML = "♦";
       document.getElementById("bot").innerHTML = "♦";
+      cardContainer.style.color = "blue";
       break;
     case 2:
       document.getElementById("up").innerHTML = "♥";
       document.getElementById("bot").innerHTML = "♥";
+      cardContainer.style.color = "red";
       break;
     case 3:
       document.getElementById("up").innerHTML = "♠";
       document.getElementById("bot").innerHTML = "♠";
+      cardContainer.style.color = "black";
       break;
     case 4:
       document.getElementById("up").innerHTML = "♣";
       document.getElementById("bot").innerHTML = "♣";
+      cardContainer.style.color = "green";
       break;
     default:
       break;
   }
-  if (0 < txtRdm && txtRdm < 10) {
-    document.getElementById("number").innerHTML = txtRdm;
+  if (0 < txtRdm && txtRdm < 9) {
+    document.getElementById("number").innerHTML = txtRdm + 1;
   } else if (txtRdm == 0) {
     document.getElementById("number").innerHTML = "A";
-  } else if (txtRdm == 10) {
+  } else if (txtRdm == 9) {
     document.getElementById("number").innerHTML = "J";
-  } else if (txtRdm == 11) {
+  } else if (txtRdm == 10) {
     document.getElementById("number").innerHTML = "Q";
-  } else if (txtRdm == 12) {
-    document.getElementById("number").innerHTML = "A";
+  } else if (txtRdm == 11) {
+    document.getElementById("number").innerHTML = "K";
   }
 };
+
+window.newButton = function newCard() {
+  let iconRdm = Math.floor(Math.random() * 4) + 1;
+  let txtRdm = Math.floor(Math.random() * 12);
+  switch (iconRdm) {
+    case 1:
+      document.getElementById("up").innerHTML = "♦";
+      document.getElementById("bot").innerHTML = "♦";
+      cardContainer.style.color = "blue";
+      break;
+    case 2:
+      document.getElementById("up").innerHTML = "♥";
+      document.getElementById("bot").innerHTML = "♥";
+      cardContainer.style.color = "red";
+      break;
+    case 3:
+      document.getElementById("up").innerHTML = "♠";
+      document.getElementById("bot").innerHTML = "♠";
+      cardContainer.style.color = "black";
+      break;
+    case 4:
+      document.getElementById("up").innerHTML = "♣";
+      document.getElementById("bot").innerHTML = "♣";
+      cardContainer.style.color = "green";
+      break;
+    default:
+      break;
+  }
+  if (0 < txtRdm && txtRdm < 9) {
+    document.getElementById("number").innerHTML = txtRdm + 1;
+  } else if (txtRdm == 0) {
+    document.getElementById("number").innerHTML = "A";
+  } else if (txtRdm == 9) {
+    document.getElementById("number").innerHTML = "J";
+  } else if (txtRdm == 10) {
+    document.getElementById("number").innerHTML = "Q";
+  } else if (txtRdm == 11) {
+    document.getElementById("number").innerHTML = "K";
+  }
+};
+
+window.setInterval(function newCard() {
+  let iconRdm = Math.floor(Math.random() * 4) + 1;
+  let txtRdm = Math.floor(Math.random() * 12);
+  switch (iconRdm) {
+    case 1:
+      document.getElementById("up").innerHTML = "♦";
+      document.getElementById("bot").innerHTML = "♦";
+      cardContainer.style.color = "blue";
+      break;
+    case 2:
+      document.getElementById("up").innerHTML = "♥";
+      document.getElementById("bot").innerHTML = "♥";
+      cardContainer.style.color = "red";
+      break;
+    case 3:
+      document.getElementById("up").innerHTML = "♠";
+      document.getElementById("bot").innerHTML = "♠";
+      cardContainer.style.color = "black";
+      break;
+    case 4:
+      document.getElementById("up").innerHTML = "♣";
+      document.getElementById("bot").innerHTML = "♣";
+      cardContainer.style.color = "green";
+      break;
+    default:
+      break;
+  }
+  if (0 < txtRdm && txtRdm < 9) {
+    document.getElementById("number").innerHTML = txtRdm + 1;
+  } else if (txtRdm == 0) {
+    document.getElementById("number").innerHTML = "A";
+  } else if (txtRdm == 9) {
+    document.getElementById("number").innerHTML = "J";
+  } else if (txtRdm == 10) {
+    document.getElementById("number").innerHTML = "Q";
+  } else if (txtRdm == 11) {
+    document.getElementById("number").innerHTML = "K";
+  }
+}, 10000);
